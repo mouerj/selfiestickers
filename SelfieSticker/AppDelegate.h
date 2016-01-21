599812fd7e768821bf7c92da13bbee7f85b66b25
mouerj <joseph.mouer@icloud.com>
2016-01-21 10:22:44 -0600

    Initial Commit

---
+++ SelfieSticker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelfieSticker
+//
+//  Created by Joseph Mouer on 1/21/16.
+//  Copyright © 2016 Joseph Mouer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelfieSticker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfieSticker
+//
+//  Created by Joseph Mouer on 1/21/16.
+//  Copyright © 2016 Joseph Mouer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
